7854ef8fe5420a3e43f9738af09984cc57ff1677
ivormyers <imyers@ucsd.edu>
2022-10-13 14:44:11 -0700

    changed Array Ex

--- ArrayTests.java
+++ ArrayTests.java
@@ -6,13 +6,18 @@ public class ArrayTests {
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+   // assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = { 5,4,3,2,1 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 1,2,3,4,5}, input2);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {4,5,6};
+    assertArrayEquals(new int[]{6,5,4}, ArrayExamples.reversed(input1));
   }
+  
 }
